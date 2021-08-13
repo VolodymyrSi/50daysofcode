d375620278b1f08a8296d19abc18eb4e3948e5dc
VolodymyrSi <volodymyr.sinevych@gmail.com>
2021-08-13 22:16:18 +0300

    fix nav

--- 3/0/script.js
+++ 3/0/script.js
@@ -2,8 +2,6 @@ const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
 
-open.addEventListener('click', () => { container.classList.add('showNav')
-})
+open.addEventListener('click', () =>  container.classList.add('showNav'))
 
-close.addEventListener('click', () => { container.classList.remove('showNav')
-})
+close.addEventListener('click', () =>  container.classList.remove('showNav'))
