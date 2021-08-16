4871a857b4813add3d331bf873b719c44e1dab6d
VolodymyrSi <volodymyr.sinevych@gmail.com>
2021-08-16 18:27:19 +0300

    add and remove classes with JS

---
+++ 6/0/script.js
@@ -0,0 +1,18 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 3.5;
+  console.log(triggerBottom);
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
+};
+
+checkBoxes();
